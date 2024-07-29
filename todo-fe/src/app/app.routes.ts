162b5df731d32e94bd253e90f6f18cec4b8359b2
Zijad Doglod <zijaddoglod01@gmail.com>
2024-07-29 12:01:30 +0200

    Development of frontend started

--- todo-fe/src/app/app.routes.ts
+++ todo-fe/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginPageComponent },
+    //{ path: 'not-found', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/not-found' }
+];
